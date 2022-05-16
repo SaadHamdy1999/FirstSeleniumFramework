4433e73c36a80fc3c64f05f9be68c14b39dbb1b6
SaadHamdy1999 <saadhamdy01999@gmail.com>
2022-05-16 12:52:46 +0200

    adding headless run to the code

--- src/test/java/tests/BaseTests.java
+++ src/test/java/tests/BaseTests.java
@@ -3,8 +3,12 @@ package tests;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriverService;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.ITestResult;
 import org.testng.annotations.*;
 import utilities.Helper;
@@ -16,14 +20,14 @@ public class BaseTests {
 
     @BeforeSuite
     @Parameters({"browser"})
-    public void setUp(@Optional("chrome") String browserName) {
+    public void setUp(@Optional("chromeheadless") String browserName) {
 
         switch (browserName.toLowerCase()) {
             case "chrome":
                 System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/drivers/chromedriver.exe");
                 driver = new ChromeDriver();
 
-            break;
+                break;
 
             case "firefox":
                 System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "/drivers/geckodriver.exe");
@@ -33,6 +37,24 @@ public class BaseTests {
                 System.setProperty("webdriver.ie.driver", System.getProperty("user.dir") + "/drivers/IEDriverServer.exe");
                 driver = new InternetExplorerDriver();
                 break;
+            case "headless":
+                DesiredCapabilities DC = new DesiredCapabilities();
+                DC.setJavascriptEnabled(true);
+                DC.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY,
+                        System.getProperty("user.dir") + "/drivers/phantomjs.exe");
+                String[] phantomArgs = {"--web-security=no","--ignore-ssl-errors=yes"};
+                DC.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS,phantomArgs);
+
+                driver =new PhantomJSDriver(DC);
+                break;
+            case"chromeheadless":
+                System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/drivers/chromedriver.exe");
+                ChromeOptions options = new ChromeOptions();
+                options.addArguments("--headless");
+                options.addArguments("--window-size=1920,1080");
+                driver =new ChromeDriver(options );
+                break;
+
         }
 
 
